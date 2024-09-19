8176c35a9f0cb5f74d088019c184515b295cf1c7
ycraah <ycraah@gmail.com>
2024-09-19 21:52:23 +0900

    "thymeleaf 출력 테스트"

--- src/main/java/org/zerock/b01/controller/SampleController.java
+++ src/main/java/org/zerock/b01/controller/SampleController.java
@@ -5,12 +5,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.Arrays;
+import java.util.List;
+
 @Controller
 @Log4j2
 public class SampleController {
-  @GetMapping("/hello")
+  @GetMapping("/ex/ex1")
   public void hello(Model model){
-    log.info("hello.....");
-    model.addAttribute("msg", "Hello World");
+    List<String> list = Arrays.asList("AAA", "BBB", "CCC", "DDD");
+    model.addAttribute("list", list);
   }
 }
