1f9a59b8915acbbcc87a8f58a25ddb114ac1ca55
ycraah <ycraah@gmail.com>
2024-09-20 00:23:40 +0900

    "템플릿 인라인 기능을 통한 JS 동작"

--- src/main/java/org/zerock/b01/controller/SampleController.java
+++ src/main/java/org/zerock/b01/controller/SampleController.java
@@ -6,7 +6,11 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 @Controller
 @Log4j2
@@ -15,5 +19,41 @@ public class SampleController {
   public void hello(Model model){
     List<String> list = Arrays.asList("AAA", "BBB", "CCC", "DDD");
     model.addAttribute("list", list);
+  }
+
+  class SampleDTO {
+    private String p1, p2, p3;
+    public String getP1(){
+      return "P1";
+    }
+    public String getP2(){
+      return "P2";
+    }
+    public String getP3(){
+      return "P3";
+    }
+  }
+
+  @GetMapping("/ex/ex2")
+  public void ex2(Model model){
+    log.info("ex/ex.......");
+    List<String> strList = IntStream.range(1, 10)
+        .mapToObj(i -> "Data" + i)
+        .collect(Collectors.toList());
+
+    model.addAttribute("list", strList);
+
+    Map<String, String> map = new HashMap<>();
+    map.put("A", "AAAA");
+    map.put("B", "BBBB");
+
+    model.addAttribute("map", map);
+
+    SampleDTO sampleDTO = new SampleDTO();
+    sampleDTO.p1 = "Value -- p1";
+    sampleDTO.p2 = "Value -- p2";
+    sampleDTO.p3 = "Value -- p3";
+
+    model.addAttribute("dto", sampleDTO);
   }
 }
