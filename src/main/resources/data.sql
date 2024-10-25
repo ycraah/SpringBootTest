a24f82d2f807b76cfa5ed04502a5d8da1c1056c2
김예찬 <ycraah@gamil.com>
2024-10-25 15:37:06 +0900

    스키마 데이í터 파일 생성

---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO COURSES(ID, NAME, CATEGORY, RATING, DESCRIPTION)
+VALUES(1, 'Rapid Spring Boot Application Development', 'Spring', 4, 'Spring Boot gives all the power of the Spring Framework without all of the complexities')
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,8 @@
+CREATE TABLE COURSES(
+    id int(15) NOT NULL,
+    name varchar(100) NOT NULL,
+    category varchar(20) NOT NULL,
+    rating int(1) NOT NULL,
+    description varchar(1000) NOT NULL,
+    PRIMARY KEY (id)
+)
